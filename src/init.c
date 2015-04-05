332583c9b115e28cdc6167c1eb1889c3e69dff68
Dirk Eddelbuettel <edd@debian.org>
2015-04-05 10:39:37 -0500

    very incomplete first commit

---
+++ src/init.c
@@ -0,0 +1,37 @@
+#include <Rconfig.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+
+/*  function declarations -- could be in external header file if used  */
+/*  by functions in anotherfile in this package                        */
+SEXP strptime(SEXP objarg, SEXP fmtarg, SEXP tzarg);
+
+
+/* definition of functions provided for .Call() 			*/
+static const R_CallMethodDef callMethods[] = {
+    { "strptime",        	(DL_FUNC) &strptime,                3 },
+    { NULL,                	NULL,                               0 }
+};
+
+
+/* functions being called when package is loaded -- used to register 	*/
+/* the functions we are exporting here					*/
+void R_init_RApiDatetime(DllInfo *info) {
+
+    /* used by external packages linking to internal package code from C */
+    R_RegisterCCallable("RApiDatetime", "strptime", (DL_FUNC) &strptime);
+
+    R_registerRoutines(info,
+                       NULL,		/* slot for .C */
+                       callMethods, 	/* slot for .Call */
+                       NULL,            /* slot for .Fortran */
+                       NULL);   	/* slot for .External */
+
+    R_useDynamicSymbols(info, TRUE);    /* controls visibility */ 
+
+}
+
+
+
+
