3d02dc9566478e89f8d74bdee44c9b3b9ce86230
Dirk Eddelbuettel <edd@debian.org>
2015-04-06 16:05:33 -0500

    simple test wrapper

---
+++ tests/test.R
@@ -0,0 +1,13 @@
+#!/usr/bin/Rscript
+
+#Sys.setenv(TZ="America/Chicago")
+options(digits.secs=6)
+library(RApiDatetime)
+x <- format(as.POSIXct(Sys.time()+0:1, origin="1970-01-01"))
+
+X <- rapistrptime(x, "%Y-%m-%d %H:%M:%OS", "") 
+print(as.POSIXct(X))    ## this works
+
+#print(str(X))		## bombs in format.POSIXlt
+#print(X) 		## bombs in format.POSIXlt
+
